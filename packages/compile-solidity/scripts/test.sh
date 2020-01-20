c0ee13bda1b98cc10691fc852c4f25bcf5876f6f
robert.magier@gmail.com <robert.magier@gmail.com>
2020-01-20 13:15:37 -0500

    20 01 2020 13 15

--- packages/compile-solidity/scripts/test.sh
+++ packages/compile-solidity/scripts/test.sh
@@ -6,5 +6,5 @@ if [ "$CI" = true ]; then
   mocha "./test/**" "./test/**/*" --timeout 10000 $@
 else
   rm -rf ./node_modules/.cache/truffle
-  mocha "./test/** ./test/**/*" --invert --grep native --timeout 10000 $@
+  mocha "./test/**" --invert --grep native --timeout 10000 $@
 fi
